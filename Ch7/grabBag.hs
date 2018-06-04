c7ed407d3454e4804c654f430e2e5ce172dc1b09
tlittrell <littrell.thomas@gmail.com>
2018-06-04 16:53:45 -0400

    test from visual studio

---
+++ Ch7/grabBag.hs
@@ -0,0 +1,20 @@
+-- grabBag.hs
+
+module GrabBag where
+
+mTh x y z = x * y * z
+
+mTh2 x y = \z -> x * y * z
+
+mTh3 x = \y -> \z -> x * y * z
+
+mTh4 = \x -> \y -> \z -> x * y * z
+
+addOneIfOdd n = case odd n of
+  True -> f n
+  False -> n
+  where f = \n -> n + 1
+
+addFive = \x -> \y -> (if x > y then y else x) + 5
+
+mflip f x y = f y x

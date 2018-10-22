5ea52583b6faa24d7c179de41630ef5e686f44ff
Thomas Littrell <Thomas.Littrell@quantumblack.com>
2018-10-22 19:29:25 -0400

    finished most ch11 exercises

--- Ch11/exercises.hs
+++ Ch11/exercises.hs
@@ -18,3 +18,27 @@ encode :: Message -> CodeWord -> String
 encode m c = map chr $ zipWith (\x y -> (x+y) `rem` 26 + ord 'A') numMessage numCode
     where numMessage = map ord $ concat $ words m
           numCode = map ord $ getCodeRepetition m c
+
+-- as-patterns
+isSubseqOf :: (Eq a) => [a] -> [a] -> Bool
+isSubseqOf [] _ = True
+isSubseqOf _ [] = False
+isSubseqOf p@(x:xs) (y:ys) = if x == y then isSubseqOf xs ys else isSubseqOf p ys
+
+isSubseqOfTests = do
+    print $ isSubseqOf "blah" "blahwoot" == True
+    print $ isSubseqOf "blah" "wootblah" == True
+    print $ isSubseqOf "blah" "wboloath" == True
+    print $ isSubseqOf "blah" "wootbla"  == False
+    print $ isSubseqOf "blah" "halbwoot" == False
+    print $ isSubseqOf "blah" "blawhoot" == True
+
+-- Language exercises
+capitalizeWord :: String -> String
+capitalizeWord (x:xs) = [toUpper x] ++ xs
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords xs = zip (words xs) (map capitalizeWord $ words xs)
+
+capitalizeWordsTests = do
+    print $ capitalizeWords "hello word"

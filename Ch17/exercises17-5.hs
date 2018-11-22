08426325dbf74081c306ca98844139ed304603bf
tlittrell <littrell.thomas@gmail.com>
2018-11-21 20:18:35 -0500

    finished ch17 section 5

--- Ch17/exercises17-5.hs
+++ Ch17/exercises17-5.hs
@@ -1,5 +1,8 @@
 import Test.QuickCheck
 import Test.QuickCheck.Property.Functor
+import Data.Monoid
+
+-- Identity
 
 newtype Identity a = Identity a deriving (Eq, Ord, Show)
 
@@ -14,3 +17,21 @@ instance (Arbitrary a) => Arbitrary (Identity a) where
 instance Applicative Identity where
     pure a = Identity a
     (<*>) (Identity f) (Identity a) = Identity (f a)
+
+-- Constant
+
+newtype Constant a b = Constant { getConstant :: a} deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+    fmap _ (Constant a) = (Constant a)
+
+instance Monoid a => Applicative (Constant a) where
+    pure _ = Constant mempty
+    (<*>) (Constant a) (Constant b) = Constant (a <> b)
+
+
+-- Fixer Upper
+fixerUpper :: IO ()
+fixerUpper = do
+    putStrLn $ show $ const <$> Just "Hello" <*> Just"world"
+    putStrLn $ show $ (,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1,2,3]
